2a6c9d54ee719d1658a4f2287e38a09aec6a65ad
Billy <armadopuka04@gmail.com>
2022-05-22 10:01:09 +0200

    Added a modal pop up on the games page & added little x-icon animation

--- website/static/main.js
+++ website/static/main.js
@@ -1,83 +1,14 @@
-// const button = document.querySelector("button"),
-//     toast = document.querySelector(".toast");
-// const closeIcon = document.querySelector(".close"),
-//     progress = document.querySelector(".progress");
+const openBtn = document.getElementById('game-config');
+const modalOverlay = document.querySelector('.game-setup-container');
+const closeSetup = document.querySelector('.game-setup-close');
+const contentBlur = document.getElementById('content');
 
-// let timer1, timer2;
-
-// button.addEventListener("click", () => {
-//     toast.classList.add("active");
-//     progress.classList.add("active");
-
-//     timer1 = setTimeout(() => {
-//         toast.classList.remove("active");
-//     }, 5000); //1s = 1000 milliseconds
-
-//     timer2 = setTimeout(() => {
-//         toast.classList.add("active");
-//         progress.classList.remove("active");
-//     }, 5300);
-// });
-
-// closeIcon.addEventListener("click", () => {
-//     toast.classList.remove("active");
-
-//     setTimeout(() => {
-//         progress.classList.remove("active");
-//     }, 300);
-
-//     clearTimeout(timer1);
-//     clearTimeout(timer2);
-// });
-
-
-
-
-// Works
-
-const closeBtn = document.getElementById('close-btn');
-const toast = document.getElementById("toast");
-
-closeBtn.addEventListener('click', () => {
-    toast.parentNode.removeChild(toast);
+openBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('open-modal');
+    contentBlur.classList.add('blur-bg');    
 });
 
-setTimeout(function() {
-    toast.parentNode.removeChild(toast);
-}, 5000);
-
-
-
-
-
-// setTimeout(function() {
-//     let text = document.getElementById("txt-hide");
-//     text.parentNode.removeChild(text);
-// }, 2000);
-
-// setTimeout(function() {
-//     let text1 = document.getElementById("txt-hide-2");
-//     text1.parentNode.removeChild(text1);
-// }, 2000);
-
-
-
-
-
-
-// $("button").click(function() {
-//     $('.alert').addClass("show");
-//     $('.alert').removeClass("hide");
-//     $('.alert').addClass("showAlert");
-
-//   setTimeout(function() {
-//     $('.alert').removeClass("show");
-//     $('.alert').addClass("hide");
-//   },5000);
-
-// });
-
-// $('.close-btn').click( function() {
-//     $('.alert').removeClass("show");
-//     $('.alert').addClass("hide");
-// });
+closeSetup.addEventListener('click', () => {
+    modalOverlay.classList.remove('open-modal');
+    contentBlur.classList.remove('blur-bg');
+});
