6e8a27740b4e8d79e0572375cbcc6125c9c18f86
Official Husko <official.husko@gmail.com>
2022-05-22 22:48:50 +0200

    Merged Code
    Fuck you billy

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_required
+from . import db
+from .models import User, UserGames
+import requests as rq
 
 views = Blueprint("views", __name__)
 
@@ -12,9 +15,36 @@ def blank():
 def home():
     return render_template("base.html", user=current_user)
 
-@views.route("/games")
+@views.route("/games", methods=["GET", "POST"])
 @login_required
 def games():
+    if request.method == "POST":
+        game_id = request.form.get("game_id")
+        game_name = request.form.get("game_name")
+        game_path = request.form.get("game_path")
+
+        game = UserGames.query.filter_by(game_id=game_id, user_id=current_user.id).first()
+        if game:
+            flash("Game was already added!", category="error")
+        elif game_id == "" or game_name == "" or game_path == "":
+            flash("Please fill out all fields.", category="error")
+        else:
+            new_game = UserGames(game_id=game_id, game_name=game_name, game_path=game_path, user_id=current_user.id)
+            print(new_game)
+            db.session.add(new_game)
+            db.session.commit()
+
+            # Retrieve image directly
+            request_image = rq.get(f"https://cdn.akamai.steamstatic.com/steam/apps/{game_id}/header.jpg")
+            if request_image.status_code == 200:
+                open(f"website/static/headers/{game_id}.jpg", 'wb').write(request_image.content)
+            else:
+                pass
+
+
+            flash("Game Added Successfully!", category="success")
+            return redirect(url_for("views.games"))
+
     return render_template("games.html", user=current_user)
 
 @views.route("/downloader")
